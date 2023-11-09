4ca928569c806d1e7e66be06feb54e0ed5cddd2e
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-09 21:58:02 +0100

    aggiunge progressi

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-const coseDaComprare= ["pane","pasta","carne","uova","latte"]
-const container = document.body
-let i = 0
-while (i < coseDaComprare.lenght) {
-    const element = coseDaComprare[i;]
-    const lista= document.createElement("p")
-    lista.innerHTML = element
-    contenitore.append(lista)
+const listaDellaSpesa  = [
+    "pane",
+    "acqua",
+    "pasta",
+    "uova",
+    "carne",
+];
+const contenitore = document.body;  
 
-    i++;
-}
+let i = 0;
+    while(i < listaDellaSpesa.length) {
+        const element = listaDellaSpesa[i];
+        console.log(element);
+        const elementHTML = document.createElement("p");
+        elementHTML.inner = element;
+        contenitore.append(elementHTML);
+        i++;
+    }
