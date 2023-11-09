646c632a332782e0113d2e22a534fc5ffcab72b7
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-09 20:50:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const coseDaComprare= ["pane","pasta","carne","uova","latte"]
+const container = document.body
+let i = 0
+while (i < coseDaComprare.lenght) {
+    const element = coseDaComprare[i;]
+    const lista= document.createElement("p")
+    lista.innerHTML = element
+    contenitore.append(lista)
+
+    i++;
+}
